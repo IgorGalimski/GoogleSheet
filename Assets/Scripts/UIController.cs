3518f73a8e5e0078895077155e2725b3e88ec66d
IgorGalimski <igorg@playtika.com>
2020-03-25 20:38:16 +0300

    Import TMP essentials

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DefaultNamespace;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] 
+    private RectTransform _loadingIndicator;
+
+    [SerializeField] 
+    private TextMeshProUGUI _logText;
+
+    private readonly GoogleDataProvider _googleDataProvider = new GoogleDataProvider();
+    
+    public async void LoadSpreadSheets()
+    {
+        _loadingIndicator.gameObject.SetActive(true);
+        
+        await _googleDataProvider.LoadSpreadsheets();
+        
+        _loadingIndicator.gameObject.SetActive(false);
+
+        _logText.text += "SpreadSpreadshets loaded" + Environment.NewLine;
+    }
+}
